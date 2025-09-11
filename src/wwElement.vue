efcdc9124e1d1c77a002df54f83271e492dbefc4
Alja Čekada <alja.cekada@gmail.com>
2025-09-11 08:45:22 +0200

    Added left borders to events

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -792,6 +792,7 @@ export default {
     :deep(.fc .fc-event.work_order) {
         /*border-left: 5px solid rgba(0, 0, 0, .28) !important;*/
         box-sizing: border-box;
+        border-left: 6px solid black !important;
     }
 
     /* lab phase = diagonal white stripes OVER the base color */
@@ -801,6 +802,7 @@ export default {
                 rgba(255, 255, 255, .18) 0 8px,
                 rgba(255, 255, 255, .34) 8px 16px) !important;
         /* overlay pattern, keeps the event's color */
+        border-left: 6px solid greenyellow !important;
     }
 
     /* ambulatory phase = dotted overlay + dashed border */
@@ -811,6 +813,7 @@ export default {
             radial-gradient(rgba(255, 255, 255, .2) 1.5px, transparent 1.5px) !important;
         background-size: 10px 10px, 10px 10px;
         background-position: 0 0, 5px 5px;
+        border-left: 6px solid red !important;
     }
 
     :deep(.fc .fc-event.ambulatory_phase) {
